97a06b05fd51d1c540e125ecbd2027d222895da5
Leila237 <mbiydzenyuyeileen@gmail.com>
2025-05-03 11:42:57 +0100

    committed changes made to the index.js router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,9 @@
 //routes/index.js
 import express from "express";
-import authMiddleware from "../middlewares/auth-middleware.js";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", authMiddleware, function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.send({ title: "Express" });
 });
 
