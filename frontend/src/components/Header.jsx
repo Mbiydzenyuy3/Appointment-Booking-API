6518eddc3bf928d1bd04f678da87e8de142756b0
leila237 <mbiydzenyuyeileen@gmail.com>
2025-12-21 20:51:10 +0100

    fix the appointment booking failure bug

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,19 +8,19 @@ export default function Header() {
 
   return (
     <>
-      <header className="bg-white shadow px-4 py-3 md:px-8 flex flex-col md:flex-row md:items-center md:justify-between mb-8">
+      <header className='bg-white shadow px-4 py-3 md:px-8 flex flex-col md:flex-row md:items-center md:justify-between mb-8'>
         <Link
-          to="/"
-          className="flex items-center space-x-2 text-2xl font-bold text-gray-800"
+          to='/'
+          className='flex items-center space-x-2 text-2xl font-bold text-gray-800'
         >
           <span>📅</span>
-          <h1 className="logo">BOOKEasy</h1>
+          <h1 className='logo'>BOOKEasy</h1>
         </Link>
 
-        <div className="mt-3 md:mt-0">
+        <div className='mt-3 md:mt-0'>
           <button
             onClick={logout}
-            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded transition"
+            className='bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded transition'
           >
             Logout
           </button>
