99685893832b72c185c1afaf2fa652f7540ce5a7
leila237 <mbiydzenyuyeileen@gmail.com>
2026-01-22 00:15:09 +0100

    added a new image

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-237f2c1f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cal1vrsnna"
+    "revision": "0.artgd1ouojg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
